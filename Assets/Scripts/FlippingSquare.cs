ba348fa2ef838b0ff363b1a2e00506857ba1eedc
Kyle <silverwolf115432@yahoo.com.au>
2021-07-22 17:05:09 +1000

    Updates to music, scene, and adding prefabs

--- Assets/Scripts/FlippingSquare.cs
+++ Assets/Scripts/FlippingSquare.cs
@@ -2,8 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScriptSpawn : MonoBehaviour
+public class FlippingSquare : MonoBehaviour
 {
+    public GameObject squareOne;
+    public GameObject squareTwo;
+    public GameObject squareThree;
+    public GameObject squareFour;
+
+    public ArrayList squares;
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/OneDestroy.cs
+++ Assets/Scripts/OneDestroy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class OneDestroy : MonoBehaviour
 {
+    public GameObject topPlatform;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +17,11 @@ public class OneDestroy : MonoBehaviour
     {
         
     }
+
+    public void SessionStart()
+    {
+        Destroy(topPlatform);
+
+    }
+
 }
